982b696a8e4a120f8faac2e65e4ba83c4760dfdc
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-09-01 18:01:52 +0000

    Thu, Sep 1, 2022, 6:01 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n;
+    do
+    {
+        n= get_int("Height: ");
+    }
+while (n<5)
+
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
 }
