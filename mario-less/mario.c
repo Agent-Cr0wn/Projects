e2c32c3d84c10eff16f081049ad1ff5a7d27a831
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-09-01 18:38:37 +0000

    Thu, Sep 1, 2022, 6:38 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n;
+    do
+    {
+        n= get_int("Height: ");
+    }
+
+    while (n < 1);
+
+
+        for (int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                printf("#");
+            }
+            printf("\n");
+        }
+
+    while(true)
+    {
+        if (n > 1)
+        {
+            break;
+        }
+    }
 }
