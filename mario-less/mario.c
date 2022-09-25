3145a044fe246ace6d26f4c4a091eb2e28887024
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-09-25 09:02:34 +0000

    Sun, Sep 25, 2022, 9:02 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -6,20 +6,20 @@ int main(void)
     int height, i, j, space;
     do
     {
-        height = get_int("Height? ");                                       //request height via input.
+        height = get_int("Height? ");                                               //request height via input.
     }
-    while(height < 1 || height > 8);                                        //only accept input between 1 and 8.
+    while(height < 1 || height > 8);                                                //only accept input between 1 and 8.
 
     for(i = 0; i < height; i++)
     {
         for(space = 0; space < height - i - 1; space++)
-        {                                                                   //print a "space" before the "#"
+        {                                                                           //print a "space" before the "#"
             printf(" ");
         }
         for(j = 0; j <= i; j++)
         {
-            printf("#");                                                    //print "#" in each line equal to number of new lines requested from input
+            printf("#");                                                            //print "#" in each line equal to number of new lines requested from input
         }
-    printf("\n");                                                           //print new line as long as "i" less than height input.
+    printf("\n");                                                                   //print new line as long as "i" less than height input.
     }
 }
