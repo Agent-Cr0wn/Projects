b7f7ad54e1ca5424685791423a15a60b9522f756
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-09-25 08:38:50 +0000

    Sun, Sep 25, 2022, 8:38 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -10,7 +10,7 @@ int main(void)
     }
     while(height < 1 || height > 8);                                        //only accept input between 1 and 8
 
-    for(i = 0; i < height; i++)                                             //
+    for(i = 0; i < height; i++)                                             
     {
         for(space = 0; space < height - i - 1; space++)
         {
