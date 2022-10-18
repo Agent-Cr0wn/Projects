2e3dae216257fb424bf3fc00bf3f93cb043a6df2
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-10-18 16:32:57 +0000

    Tue, Oct 18, 2022, 4:32 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -31,6 +31,20 @@ int main(void)
     float L = letters / (float)words * 100
     float S = sentences / (float)words * 100
 
-    int index = round(0.0588)
+    int index = round(0.0588 * L - 0.296 * S - 15.8)
 
+    if (index < 1)
+    {
+        printf("Before Grade 1");
+    }
+
+    else if (index > 16)
+    {
+        printf("Grade 16+");
+    }
+
+    else
+    {
+        printf("Grade %i", index);
+    }
 }
