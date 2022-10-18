44df763af846a6eb2d493ec14f98c6822e3ebf3b
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-10-18 16:01:32 +0000

    Tue, Oct 18, 2022, 4:01 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,13 +1,22 @@
+#include <ctype.h>
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
     string text = get_string("Text: ");
-    
+
+    int letters = 0;
+    int words = 0;
+    int sentences = 0;
 
     for (i = 0; i < strlen(text); i++)
     {
-        if
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+
+        
     }
 }
