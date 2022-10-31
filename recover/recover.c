2d02148bf83777b874fa93b51ca723bb678eacc2
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-10-31 16:00:48 +0000

    Mon, Oct 31, 2022, 4:00 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    typedef uint8_t BYTE;
+    BYTE buffer [512];
+    int bytes_read, counter = 0;
+    char filename[8];
+    // Open memory card
+    FILE *f = fopen(argv[1], "r");
+    // Repeat until the end of the card
+    while(true)
+    {
+    // Read 512 bytes into a buffer
+        bytes_read = fread(buffer, sizeof(BYTE), 512, f);
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
+        {
+    // If first JPEG then create file and write
+            if(counter == 0)
+            {
+
+                sprintf(filename, "%03i.jpg", 2, count);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
 }
