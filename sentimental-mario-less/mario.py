b2bb186dc4aefbbfb8709f2d582f4a0e070235a5
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-02 17:01:35 +0000

    Wed, Nov 2, 2022, 5:01 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -8,8 +8,8 @@ while True:
 for i in range(0, n, 1):
     for j in range(0, n, 1):
         if(i+j < n-1):
-            print(" ")
+            print(" ", end="")
         else:
-            print("#")
+            print("#", end="")
     print()
 
