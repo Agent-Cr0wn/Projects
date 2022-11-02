cd2fd2ecb5e36cfe1c0ad14d5b683219344255aa
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-02 17:14:59 +0000

    Wed, Nov 2, 2022, 5:14 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,3 +5,27 @@ while True:
     if cents > 0:
         break
 
+count = 0
+
+# Quarters
+while cents >= 25:
+    cents = cents - 15
+    count += 1
+
+# Dimes
+while cents >= 10:
+    cents = cents - 10
+    count += 1
+
+# Nickels
+while cents >= 5:
+    cents = cents - 5
+    count += 1
+
+# Pennies
+while cents >= 1:
+    cents = cents - 1
+    count += 1
+
+
+
