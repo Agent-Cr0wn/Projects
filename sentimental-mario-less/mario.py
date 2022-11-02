0a703aa153ef5e0ec0d101dde5f66e3806a0367f
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-02 16:55:19 +0000

    Wed, Nov 2, 2022, 4:55 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,4 +1,11 @@
 from cs50 import get_int
 
-while (height < 1 and height > 8):
-    height = get_int("Height: ")
+while True:
+    n = get_int("Height: ")
+    if n > 0 and n < 9:
+        break
+
+for i in range(0, n, 1):
+    for j in range(0, n, 1):
+        if()
+
