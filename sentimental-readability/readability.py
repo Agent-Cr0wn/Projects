d4520baeabe1a79ab071d40a5d6682219b9ce774
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-04 14:43:15 +0000

    Fri, Nov 4, 2022, 2:43 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -6,11 +6,18 @@ letters = 0
 words = 1
 sentences = 0
 
-for i in range(len(text[i])):
-    if text[i] == text.isalpha():
-        letters++
-    elif text[i] == " ":
-        words++
+for i in text:
+    if i.isalpha():
+        letters += 1
+    elif i == " ":
+        words += 1
+    elif i == "." or i == "?" or i == "!":
+        sentences += 1
+
+index = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
+
+if index < 1:
+    print("Before ")
 
 
 
