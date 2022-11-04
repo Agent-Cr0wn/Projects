8387f2edc44002d15eebc79794983a98ddb8f8ae
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-04 14:45:00 +0000

    Fri, Nov 4, 2022, 2:45 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -17,7 +17,12 @@ for i in text:
 index = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
 
 if index < 1:
-    print("Before ")
+    print("Before Grade 1")
+elif index > 16:
+    print("Grade 16+")
+else:
+    print("Grade", round(index))
+
 
 
 
