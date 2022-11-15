737e31222b9828f66ec00488abd4f75f7849f0ec
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-15 16:10:40 +0000

    Tue, Nov 15, 2022, 4:10 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -15,7 +15,7 @@ def main():
         sys.exit("Usage: python tournament.py FILENAME")
 
     teams = []
-    # TODO: Read teams into memory from file
+    # Read teams into memory from file
     filename = sys.argv[1]
     with open(filename) ad file:
         reader = csv.DictReader(file)
@@ -24,8 +24,14 @@ def main():
             teams.append(team)
 
     counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
-    
+    # Simulate N tournaments and keep track of win counts
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] =1
+
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -57,6 +63,9 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    return teams[0]["team"]
 
 
 if __name__ == "__main__":
