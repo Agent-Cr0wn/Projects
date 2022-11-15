dee46126d56ec458e3e2df067f443b5fcc6cf5c6
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-15 16:44:15 +0000

    Tue, Nov 15, 2022, 4:44 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -9,12 +9,18 @@ def main():
         sys.exit("Usage : python dna.py data.csv sequence.txt")
 
     # Read database file into a variable
-    
+    database = []
+    with open(sys.argv[1], "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            database.append(row)
 
     # Read DNA sequence file into a variable
+    with open(sys.argv[2], "r") as file:
+        dna_sequence = file.read()
 
     # Find longest match of each STR in DNA sequence
-
+    
     # Check database for matching profiles
 
     return
