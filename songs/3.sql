d0d431462e6416bf3dcd8fbf0caad29524ced86d
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-26 11:32:34 +0000

    Sat, Nov 26, 2022, 11:32 AM +00:00

--- songs/3.sql
+++ songs/3.sql
@@ -1,6 +1,3 @@
--- SELECT name FROM songs
--- ORDER BY duration_ms
--- DESC LIMIT 5;
-
-SELECT Top(5) name FROM songs
-ORDER BY duration_ms DESC;
+SELECT name FROM songs
+ORDER BY duration_ms
+DESC LIMIT 5;
