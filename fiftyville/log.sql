f87fddb5cb6ff6c3a0dcf4e5e632048462046ac6
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-29 18:12:18 +0000

    Tue, Nov 29, 2022, 6:12 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -78,4 +78,26 @@ WHERE (flights.year = 2021
         AND flights.month = 7
         AND flights.day = 29
         AND flights.id = 36)
-
+AND NAME IN
+(SELECT phone_calls.caller_name FROM phone_calls
+WHERE year = 2021
+    AND month = 7
+    AND day = 28
+    AND duration < 60)
+AND name IN
+(SELECT people.name FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
+WHERE atm_transactions.year = 2021
+    AND atm_transactions.month = 7
+    AND atm_transactions.day = 28
+    AND atm_location = "Leggett Street"
+    AND atm_transactions.transaction_type = "withdraw")
+AND name IN
+(JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+WHERE bakery_security_logs.year = 2021
+    AND bakery_security_logs.month = 7
+    AND bakery_security_logs.day = 28
+    AND bakery_security_logs.hour = 10
+    AND bakery_security_logs.minute >= 15
+    AND bakery_security_logs.minute <= 25)
