552c3efb24ecaee5a9579f7b47165eadb5d25edc
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-29 16:43:53 +0000

    Tue, Nov 29, 2022, 4:43 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -25,7 +25,7 @@ SELECT people.name, atm_transactions.transaction_type FROM people
 JOIN bank_accounts ON bank_accounts.person_id = people.id
 JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
 WHERE atm_transaction.year = 2021
-    AND atm_transaction.month = 7
-    AND atm_transaction.day = 28
+    AND atm_transactions.month = 7
+    AND atm_transactions.day = 28
     AND atm_location = "Leggett Street"
-    AND atm_transaction.transaction_type = "withdraw";
+    AND atm_transactions.transaction_type = "withdraw";
