23df8fb6be993696e5bc6391dd5b1a3eb63d18a1
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-29 17:08:03 +0000

    Tue, Nov 29, 2022, 5:08 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -36,7 +36,7 @@ WHERE year = 2021
     AND day = 28
     AND duration < 60;
 
-SELECT people.name, people.phone_number, phone_calls.caller, phone_calls.receiver, people.name FROM people
+SELECT people.name, people.phone_number, phone_calls.caller, phone_calls.receiver, FROM people
 JOIN phone_calls ON phone_calls.caller = people.phone_number
 WHERE year = 2021
     AND month = 7
