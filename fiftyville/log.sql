967434fe4bd0fac62e7bb9e69557da75a8c3f6fa
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2022-11-29 16:23:03 +0000

    Tue, Nov 29, 2022, 4:23 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,11 +4,11 @@ SELECT description FROM crime_scene_reports
 WHERE year = 2021
     AND month = 7
     AND day = 28;
--- 
-
+-- 3 witnesses / theft time = 10:15am / "bakery"
 
 SELECT transcript FROM interviews
 WHERE year = 2021
     AND month = 7
     AND day = 28
 AND transcript LIKE "%bakery%";
+--
