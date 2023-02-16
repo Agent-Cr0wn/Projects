44ffbca42dbe41942fcfea70a35a59e4afebfa66
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2023-02-16 16:01:04 +0000

    Thu, Feb 16, 2023, 6:01 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -122,7 +122,7 @@ def quote():
         if stock == None:
             return apology("Symbol Doesn't Exist")
 
-        return render_template("quoted.html", name = stock["name"], price = stock["price"], symbol = )
+        return render_template("quoted.html", name = stock["name"], price = stock["price"], symbol = stock["symbol"])
 
 
 @app.route("/register", methods=["GET", "POST"])
