67e17181cc894af281999439b12e96cec13084b8
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2023-03-01 17:15:15 +0000

    Wed, Mar 1, 2023, 7:15 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -357,9 +357,9 @@ def change_password():
         # Get user info from database
         user_id = session["user_id"]
         user_db = db.execute("SELECT * FROM users WHERE id = :id", id=user_id)
-        
+
         # Ensure all fields are filled out
-        if not old_password or not new_password or not confirm_password:
+        if not (old_password and new_password and confirm_password):
             return apology("Please fill out all fields")
 
         # Ensure new password and confirmation match
