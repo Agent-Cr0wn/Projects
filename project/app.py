bf0d2d9b2fb1d3f3f345a9d02530a2d2df47ea2d
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2023-03-12 14:18:49 +0000

    Sun, Mar 12, 2023, 4:18 PM +02:00

--- project/app.py
+++ project/app.py
@@ -1,44 +1,90 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myblog.db'
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'secret-key'
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+# Dummy data for blog posts
+POSTS = [
+    {
+        'title': 'First post',
+        'content': 'This is the content of the first post.',
+        'author': 'John Doe',
+        'timestamp': datetime.strptime('2022-01-01', '%Y-%m-%d')
+    },
+    {
+        'title': 'Second post',
+        'content': 'This is the content of the second post.',
+        'author': 'Jane Doe',
+        'timestamp': datetime.strptime('2022-01-02', '%Y-%m-%d')
+    }
+]
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    author = db.relationship('User', backref=db.backref('posts', lazy=True))
+# Helper function to get a post by its title
+def get_post(title):
+    for post in POSTS:
+        if post['title'] == title:
+            return post
+    return None
 
 @app.route('/')
 def index():
-    posts = Post.query.all()
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', posts=POSTS)
+
+@app.route('/post/<title>')
+def post(title):
+    post = get_post(title)
+    if not post:
+        flash('Post not found!')
+        return redirect(url_for('index'))
+    return render_template('post.html', post=post)
+
+@app.route('/create_post', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        author = session['username']
+        timestamp = datetime.now()
+        POSTS.append({
+            'title': title,
+            'content': content,
+            'author': author,
+            'timestamp': timestamp
+        })
+        flash('Post created successfully!')
+        return redirect(url_for('index'))
+    return render_template('create_post.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Handle user login
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        for user in USERS:
+            if user['username'] == username and check_password_hash(user['password'], password):
+                session['username'] = username
+                session['logged_in'] = True
+                flash('Logged in successfully!')
+                return redirect(url_for('index'))
+        flash('Incorrect username or password!')
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    # Handle user registration
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        password_hash = generate_password_hash(password)
+        USERS.append({'username': username, 'password': password_hash})
+        flash('Registered successfully!')
+        return redirect(url_for('login'))
     return render_template('register.html')
 
-@app.route('/create_post', methods=['GET', 'POST'])
-@login_required
-def create_post():
-    # Handle blog post creation
-    return render_template('create_post.html')
-
-@app.route('/post/<int:post_id>')
-@login_required
-def post(post_id):
-    post = Post.query.get
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session['logged_in'] = False
+    flash('Logged out successfully!')
+    return redirect(url_for('index'))
