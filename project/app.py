78a6e218f40875e376fb27b41d9dba69d5a43e99
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2023-03-12 14:04:56 +0000

    Sun, Mar 12, 2023, 4:04 PM +02:00

--- project/app.py
+++ project/app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('User', backref=db.backref('posts', lazy=True))
