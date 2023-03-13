80fba6c755cca3448dbfcada309b2fe8af9e4f02
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2023-03-13 16:04:17 +0000

    Mon, Mar 13, 2023, 6:04 PM +02:00

--- project/app.py
+++ project/app.py
@@ -11,5 +11,5 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+# Configure to use SQLite database
 db = SQL("sqlite:///users.db")
