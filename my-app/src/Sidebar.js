a5a9c39d48180f92add05ae022caea5cf9652106
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2023-03-27 17:48:01 +0000

    Mon, Mar 27, 2023, 7:48 PM +02:00

--- my-app/src/Sidebar.js
+++ my-app/src/Sidebar.js
@@ -1,6 +1,6 @@
 const Sidebar = () => {
     return (
-        <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 ">
+        <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-green-600">
             <i>Home</i>
             <i>About Me</i>
             <i>Resume</i>
