3d7f3406a722bcb534c226aa8c9ea57f500ffefd
Agent-Cr0wn <112572767+Agent-Cr0wn@users.noreply.github.com>
2023-03-29 16:53:22 +0000

    Wed, Mar 29, 2023, 6:53 PM +02:00

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+import Main from './components/Main'
 
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
+      <Main />
     </div>
   );
 }
